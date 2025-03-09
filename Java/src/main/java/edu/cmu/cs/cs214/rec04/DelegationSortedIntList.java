72764244211ddb95ef9235f355f9b978606b5326
Tumen-Amgalan <tumee2045@gmail.com>
2025-03-09 14:34:01 +0800

    B232270063

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -10,7 +10,43 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class DelegationSortedIntList {
-    // Write your implementation below with API documentation
+public class DelegationSortedIntList implements IntegerList {
+    private final SortedIntList sortedList = new SortedIntList();
+    private int totalAdded = 0;
 
+    @Override
+    public boolean add(int num) {
+        totalAdded++;
+        return sortedList.add(num);
+    }
+
+    @Override
+    public boolean addAll(IntegerList list) {
+        totalAdded += list.size();
+        return sortedList.addAll(list);
+    }
+
+    @Override
+    public int get(int index) {
+        return sortedList.get(index);
+    }
+
+    @Override
+    public int size() {
+        return sortedList.size();
+    }
+
+    public int getTotalAdded() {
+        return totalAdded;
+    }
+
+    @Override
+    public boolean remove(int num) {
+        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+    }
+
+    @Override
+    public boolean removeAll(IntegerList list) {
+        throw new UnsupportedOperationException("Unimplemented method 'removeAll'");
+    }
 }
